3cd0727db7d36b6f5be0223d07156a5a480d76c2
bonfacethaa <thaabonface@gmail.com>
2022-01-04 10:02:37 +0300

    feat(core): Add password reset views, urls and templates

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -37,7 +37,12 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(success_url='/')),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(success_url='/'), name='password_change'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register', register, name='register'),
 ]
